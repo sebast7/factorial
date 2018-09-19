b865b908be818c02350f978b95c2bfd8fa893ef2
sebast7 <43425589+sebast7@users.noreply.github.com>
2018-09-19 18:58:36 -0500

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,25 @@
+package calculadora;
+ 
+/**
+ * 
+ * @author Sebastian Beltran
+ *
+ */
+
+public class factorial {
+	/**
+	 * 
+	 * @param This program performs a factorial operation through recursion
+	 * @return
+	 */
+	public static int factorial (int numero) 
+	{
+		if (numero > 0) 
+			return numero * factorial(numero-1);
+		else
+			return 1;
+		}
+	public static void main(String[] args) {
+		System.out.println("Factorial de 4 es: " + factorial(4));
+		}
+	}
