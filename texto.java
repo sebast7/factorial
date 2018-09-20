8fd2b38ad49c7dac6cbc005c694b823f58b2e008
Sebastian Beltran <43425589+sebast7@users.noreply.github.com>
2018-09-19 22:23:25 -0500

    Add files via upload

---
+++ texto.java
@@ -0,0 +1,42 @@
+package calculadora;
+import java.io.*;
+
+/**
+* 
+* @author Sebastian Beltran
+*
+*/
+
+public class texto {
+	/**
+	 * 
+	 * @param this program tells us how many times a letter or a word is repeated
+	 * @return
+	 */
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader ( new InputStreamReader (System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+		String Texto;
+		String palabra;
+		int cont=0;
+		try
+		{
+			System.out.println("write the text: " );
+			Texto=br.readLine();
+			System.out.println("write the word or letter you want to find: " );
+			palabra =br.readLine(); 
+			while (Texto.indexOf(palabra) > -1) 
+			{
+				Texto = Texto.substring(Texto.indexOf(palabra)+palabra.length(),Texto.length());
+				cont++; 
+			}
+			System.out.println ( "the letter or word " + palabra + " is repeated " + cont + " times");
+			bw.flush();
+	    }
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
