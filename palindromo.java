398548edf94cee935a1ea570e33c448b8312cdd8
sebast7 <43425589+sebast7@users.noreply.github.com>
2018-09-19 21:17:59 -0500

    Add files via upload

---
+++ palindromo.java
@@ -0,0 +1,51 @@
+package calculadora;
+import java.io.*;
+
+ 
+/**
+ * 
+ * @author Sebastian Beltran
+ *
+ */
+
+public class palindromo {
+	/**
+	 * 
+	 * @param this program performs the fibonacci series recursively
+	 * @return
+	 */
+	public static boolean palindromo (String frase) 
+	{
+		for (int i = 0; i < frase.length(); i++) 
+		{
+			if(frase.charAt(i) != frase.charAt(frase.length()-i -1))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) 
+	{
+		BufferedReader br = new BufferedReader ( new InputStreamReader (System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+		String frase;
+		try
+		{
+			bw.write("escriba la palabara");
+			frase=br.readLine();
+			if(palindromo(frase)== true)
+			{
+				System.out.println("la frase es palindromo" + frase);
+			}
+			else
+			{
+				System.out.println("la frase no es palindromo" + frase);
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
