ca3ae5d7fb54f094e35f53203cc34c31570d7ff9
sebast7 <43425589+sebast7@users.noreply.github.com>
2018-09-19 19:24:59 -0500

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,34 @@
+package calculadora;
+ 
+/**
+ * 
+ * @author Sebastian Beltran
+ *
+ */
+
+public class fibonacci {
+	/**
+	 * 
+	 * @param this program performs the fibonacci series recursively
+	 * @return
+	 */
+	public static int fibonacci (int numero) 
+	{
+		if(numero == 0) 
+			return 1;
+		else
+			if(numero == 1)
+				return 1;
+			else
+				return fibonacci (numero-1) + fibonacci (numero-2);
+		}
+	public static void main(String[] args) 
+	{
+		int n = 0;
+		while(n!=9)
+		{
+			System.out.println("la serie de fibonacci es : " + n + " es " + fibonacci (n));
+			n++;
+		}
+	}
+}
