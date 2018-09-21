a8c5a9e7b4be3cd291e2307ac15f1855aa5cdbd8
Sebastian Beltran <43425589+sebast7@users.noreply.github.com>
2018-09-21 08:17:17 -0500

    Update texto.java

--- texto.java
+++ texto.java
@@ -8,24 +8,30 @@ import java.io.*;
 */
 
 public class texto {
+	
 	/**
 	 * 
 	 * @param this program tells us how many times a letter or a word is repeated
 	 * @return
 	 */
+	
 	public static void main(String[] args) {
 		
 		BufferedReader br = new BufferedReader ( new InputStreamReader (System.in));
 		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+		
 		String Texto;
 		String palabra;
 		int cont=0;
+		
 		try
 		{
 			System.out.println("write the text: " );
 			Texto=br.readLine();
+			
 			System.out.println("write the word or letter you want to find: " );
 			palabra =br.readLine(); 
+			
 			while (Texto.indexOf(palabra) > -1) 
 			{
 				Texto = Texto.substring(Texto.indexOf(palabra)+palabra.length(),Texto.length());
