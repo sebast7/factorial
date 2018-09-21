77d33f50856b98954a4bdf22d0c52e09499fdd72
Sebastian Beltran <43425589+sebast7@users.noreply.github.com>
2018-09-21 08:15:47 -0500

    Update palindromo.java

--- palindromo.java
+++ palindromo.java
@@ -1,7 +1,6 @@
 package taller;
 import java.io.*;
 
- 
 /**
  * 
  * @author Sebastian Beltran
@@ -14,6 +13,7 @@ public class palindromo {
 	 * @param this program tells us if the phrase is palindromic or not
 	 * @return
 	 */
+	
 	public static boolean palindromo (String frase) 
 	{
 		for (int i = 0; i < frase.length(); i++) 
@@ -27,13 +27,17 @@ public class palindromo {
 	}
 	public static void main(String[] args) 
 	{
+		
 		BufferedReader br = new BufferedReader ( new InputStreamReader (System.in));
 		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+		
 		String frase;
+		
 		try
 		{
-			System.out.println("write the word");
+			bw.write("escriba la palabara");
 			frase=br.readLine();
+			
 			if(palindromo(frase)== true)
 			{
 				System.out.println("the phrase is a palindrome" + frase);
